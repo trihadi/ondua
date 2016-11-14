da28db944c13e28491d1302b335f47029fed1f1f
kosim21 <kdw.kzm@gmail.com>
2016-11-15 01:42:49 +0700

    Update server_api.go

--- server/server_api.go
+++ server/server_api.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/anacrolix/torrent/metainfo"
 
-	"github.com/ipadtechspot/iTS-torrent/engine"
+	"github.com/kosim21/kosim-torrent/engine"
 )
 
 func (s *Server) api(r *http.Request) error {
