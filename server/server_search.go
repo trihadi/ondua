e7dd52e510926aa796028e4e0eb352f40677b6a0
kosim21 <kdw.kzm@gmail.com>
2017-01-11 03:46:26 +0700

    Update server_search.go

--- server/server_search.go
+++ server/server_search.go
@@ -29,23 +29,17 @@ var defaultSearchConfig = []byte(`{
 			"peers": "td:nth-child(4)"
 		}
 	},
-	"abb": {
-		"name": "The Audiobook Bay",
-		"url": "http://audiobookbay.me/page/{{page:1}}/?s={{query}}",
-		"list": "#content > div",
+        "et": {
+		"name": "ExtraTorrent",
+		"url": "https://extratorrent.cc/search/?search={{query}}&s_cat=&pp=&srt=seeds&order=desc&page={{page:1}}",
+		"list": "table.tl tr.tlr",
 		"result": {
-			"name":["div.postTitle > h2 > a","@title"],
-			"path":["div.postTitle > h2 > a","@href"],
-			"seeds": "div.postContent > p:nth-child(3) > span:nth-child(1)",
-			"peers": "div.postContent > p:nth-child(3) > span:nth-child(3)"
-		}
-	},
-	"abb-item": {
-		"name": "The Audiobook Bay (Item)",
-		"url": "http://audiobookbay.me{{path}}",
-		"result": {
-			"infohash": "/td>([a-f0-9]+)</",
-			"tracker": "table tr td:nth-child(2)"
+			"name":["td.tli > a"],
+			"torrent": ["td:nth-child(1) a","@href","s/torrent_download/download/"],
+			"url": ["td.tli > a","@href"],
+			"size": "td:nth-child(5)",
+			"seeds": "td.sy",
+			"peers": "td.ly"
 		}
 	}
 }`)
