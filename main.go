22fbbd88ca29243e87ea2053e07b2c92f1445d0a
kosim21 <kdw.kzm@gmail.com>
2017-01-24 17:42:35 +0700

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ var VERSION = "0.0.0-src" //set with ldflags
 func main() {
 	s := server.Server{
 		Title:      "Click here to save on Ur Gdrive",
-		Port:       5000,
+		Port:       3000,
 		ConfigPath: "kzm.json",
 	}
 
